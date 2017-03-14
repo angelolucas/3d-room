e5362c1376b8b0dc528eb9b171648a1c2379395c
Ângelo Lucas <angelo.lrc@gmail.com>
2017-03-14 11:41:58 -0300

    basic 3d room

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import './App.css';
+import './room.css';
+
 class App extends Component {
   render() {
     return (
-      <div class="room">
-        <div class="room__floor"></div>
-        <div class="room__ceiling"></div>
-        <div class="room__wall room__wall--north"></div>
-        <div class="room__wall room__wall--south"></div>
-        <div class="room__wall room__wall--east"></div>
-        <div class="room__wall room__wall--west"></div>
+      <div className="room">
+        <div class="room__perspective">
+          <div className="room__container">
+            <div className="room__floor"></div>
+            <div className="room__ceiling"></div>
+            <div className="room__wall room__wall--north"></div>
+            <div className="room__wall room__wall--south"></div>
+            <div className="room__wall room__wall--east"></div>
+            <div className="room__wall room__wall--west"></div>
+          </div>
+        </div>
       </div>
     );
   }
