2811fe9bd345add880fcd6dd3820b48d77774a66
Ângelo Lucas <angelo.lrc@gmail.com>
2017-03-15 17:03:36 -0300

    build config

--- config/paths.js
+++ config/paths.js
@@ -1,8 +1,5 @@
-'use strict';
-
 var path = require('path');
 var fs = require('fs');
-var url = require('url');
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebookincubator/create-react-app/issues/637
@@ -22,46 +19,10 @@ function resolveApp(relativePath) {
 // It will then be used by Webpack configs.
 // Jest doesn’t need this because it already handles `NODE_PATH` out of the box.
 
-// Note that unlike in Node, only *relative* paths from `NODE_PATH` are honored.
-// Otherwise, we risk importing Node.js core modules into an app instead of Webpack shims.
-// https://github.com/facebookincubator/create-react-app/issues/1023#issuecomment-265344421
-
 var nodePaths = (process.env.NODE_PATH || '')
   .split(process.platform === 'win32' ? ';' : ':')
   .filter(Boolean)
-  .filter(folder => !path.isAbsolute(folder))
   .map(resolveApp);
-
-var envPublicUrl = process.env.PUBLIC_URL;
-
-function ensureSlash(path, needsSlash) {
-  var hasSlash = path.endsWith('/');
-  if (hasSlash && !needsSlash) {
-    return path.substr(path, path.length - 1);
-  } else if (!hasSlash && needsSlash) {
-    return path + '/';
-  } else {
-    return path;
-  }
-}
-
-function getPublicUrl(appPackageJson) {
-  return envPublicUrl || require(appPackageJson).homepage;
-}
-
-// We use `PUBLIC_URL` environment variable or "homepage" field to infer
-// "public path" at which the app is served.
-// Webpack needs to know it to put the right <script> hrefs into HTML even in
-// single-page apps that may serve index.html for nested URLs like /todos/42.
-// We can't use a relative path in HTML because we don't want to load something
-// like /todos/42/static/js/bundle.7289d.js. We have to know the root.
-function getServedPath(appPackageJson) {
-  var publicUrl = getPublicUrl(appPackageJson);
-  var servedUrl = envPublicUrl || (
-    publicUrl ? url.parse(publicUrl).pathname : '/'
-  );
-  return ensureSlash(servedUrl, true);
-}
 
 // config after eject: we're in ./config/
 module.exports = {
@@ -74,7 +35,6 @@ module.exports = {
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveApp('src/setupTests.js'),
   appNodeModules: resolveApp('node_modules'),
-  nodePaths: nodePaths,
-  publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json'))
+  ownNodeModules: resolveApp('node_modules'),
+  nodePaths: nodePaths
 };
--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,12 +1,12 @@
 // Export the PostCSS Config for usage in webpack
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-     
-    /* Use tomorrow’s CSS syntax, today. */
-    require('postcss-cssnext'),
+	plugins: [
+		require("postcss-import"),
+		 
+		/* Use tomorrow’s CSS syntax, today. */
+		require('postcss-cssnext'),
 
-    /* Enable nested css selectors like Sass/Less */
-    require('postcss-nested')
-  ]
+		/* Enable nested css selectors like Sass/Less */
+		require('postcss-nested')
+	]
 }
